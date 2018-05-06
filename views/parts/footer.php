ebe2938236fcc2f3a28a394a78e6076c92560614
Artur1312 <arturplay007@gmail.com>
2018-05-06 15:53:30 +0300

    front including.

---
+++ views/parts/footer.php
@@ -0,0 +1,10 @@
+<?php
+
+Use yii\helpers\Url;
+?>
+<footer class="footer">
+    <div class="container-fluid clearfix">
+        <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © <?= date('Y') ?> By <a href="#" target="_blank">Airy</a>. All rights reserved.</span>
+        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="mdi mdi-heart text-danger"></i></span>
+    </div>
+</footer>
