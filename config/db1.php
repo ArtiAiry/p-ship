ccb5f78311f8e94a7edaa01b6315de727a68d1aa
ArtiAiry <31469227+ArtiAiry@users.noreply.github.com>
2018-08-20 12:29:32 +0300

    Update db1.php

--- config/db1.php
+++ config/db1.php
@@ -2,13 +2,13 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=db21.freehost.com.ua;dbname=airyschoo_zoxmax',
-    'username' => 'airyschoo_zoxmax',
-    'password' => '1ItXJMGtE',
+    'dsn' => 'mysql:host=db21.freehost.com.ua;dbname=*******',
+    'username' => '*******',
+    'password' => '*******',
     'charset' => 'utf8',
-
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
     //'schemaCache' => 'cache',
+
 ];
